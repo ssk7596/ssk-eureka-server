9772321bc2f0d2b6550406267d0798b3ec6ebeb5
ssk7596 <shubhankar7596@gmail.com>
2020-10-06 00:19:13 +0530

    eureka server changes

---
+++ src/test/java/com/ssk/eurekaserver/SskEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssk.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SskEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
